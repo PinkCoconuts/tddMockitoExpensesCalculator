d297af9731127faef7e6d9f35aaa1b372b94c9f1
Madalina <cristina.madalina.dragan@gmail.com>
2016-05-22 14:58:13 +0200

    IMPLEMENT: add category and update category functionality

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -156,7 +156,15 @@ public class Controller {
     public boolean updateCategory( String stringID, String name ) {
         int id = Integer.parseInt( stringID );
         Category category = new Category( id, name );
-        if ( facade.updateCategory(id, category ) != null ) {
+        if ( facade.updateCategory( id, category ) != null ) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCategory( String categoryName ) {
+        Category category = new Category( 0, categoryName );
+        if ( facade.insertCategory(category ) != null ) {
             return true;
         }
         return false;
